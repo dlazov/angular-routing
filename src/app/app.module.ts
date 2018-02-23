9c793c0a0c50d75ce5a922d1bb49867f03c21735
Don Lazov <dlazov@zovs-redhat.zovsdomain>
2018-02-23 12:30:15 -0500

    Added admin module, routing and admin/email and admin/user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CustomerComponent } from './customer/customer.component';
-
+import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [
@@ -16,6 +16,7 @@ import { CustomerComponent } from './customer/customer.component';
   ],
   imports: [
     BrowserModule,
+    AdminModule,
     AppRoutingModule
   ],
   providers: [],
