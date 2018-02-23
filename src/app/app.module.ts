d01e79b1664e6d0270dd1f9794e66402bcd6c4a4
Don Lazov <dlazov@zovs-redhat.zovsdomain>
2018-02-23 15:45:44 -0500

    added material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatButtonModule, MatIcon, MatIconModule } from '@angular/material';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CustomerComponent } from './customer/customer.component';
@@ -16,6 +17,8 @@ import { AdminModule } from './admin/admin.module';
   ],
   imports: [
     BrowserModule,
+    MatButtonModule,
+    MatIconModule,
     AdminModule,
     AppRoutingModule
   ],
